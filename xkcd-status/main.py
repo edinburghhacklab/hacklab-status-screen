e7df2f37a4bf8f277dbd18838f30af699fee7780
Pi <pi@bus-timetable-pi.hacklab>
2024-08-25 20:15:20 +0100

    move xkcd-status into bus-timetables dir

---
+++ xkcd-status/main.py
@@ -0,0 +1,37 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from requests import get
+import io
+
+hostName = "localhost"
+serverPort = 2578
+
+class WebServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            site: str
+            with open("index.html") as f:
+                site = f.read()
+
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes(site, "utf-8"))
+        elif self.path == "/api":
+            obj = get("https://xkcd.com/info.0.json").text
+
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(bytes(obj, "utf-8"))
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), WebServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
