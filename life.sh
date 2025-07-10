d7e66936f8821b1a74b57876e61dbe94435899bb
Pi <pi@bus-timetable-pi.hacklab>
2025-07-10 08:36:44 +0100

    Improve life script

--- life.sh
+++ life.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 TMP_FILE=$(mktemp)
 cat /dev/fb0 > $TMP_FILE
-/home/pi/life-framebuffer/a.out &
-sleep 5
-kill $!
+sudo chvt 10
 cat $TMP_FILE > /dev/fb0
 rm $TMP_FILE
+/home/pi/life-framebuffer/a.out &
+sleep 5
+kill -9 $!
+sudo chvt 2
+# TODO get chrome to refresh itself
+xrefresh
